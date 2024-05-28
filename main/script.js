c48cf60639f551077267b807c474699bbb6c605c
Shifu-in <165253001+Shifu-in@users.noreply.github.com>
2024-05-29 05:08:54 +0700

    Update script.js

--- main/script.js
+++ main/script.js
@@ -1,1 +1,15 @@
+var balanceElement = document.getElementById("balance");
+var countElement = document.getElementById("count");
 
+var countUpOptions = {
+    separator: " "
+};
+
+var count = new CountUp(countElement, 0.00022793, 0.0101595600, 13, 1, countUpOptions);
+
+document.getElementById("startButton").addEventListener("click", function() {
+    count.start(function() {
+        var newBalance = parseFloat(balanceElement.textContent) + parseFloat(count.endVal);
+        balanceElement.textContent = newBalance.toFixed(8);
+    });
+});
