f6e39a31c201fcae32b33e196b1337a476ff3c95
Shifu-in <165253001+Shifu-in@users.noreply.github.com>
2024-05-29 05:18:27 +0700

    Update script.js

--- partners/script.js
+++ partners/script.js
@@ -1,1 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var balanceElement = document.getElementById("balance");
+    var balance = 0;
+    var subscribedChannels = new Set();
 
+    document.querySelectorAll('.subscribe-button').forEach(function(button) {
+        button.addEventListener('click', function() {
+            var channel = button.getAttribute('data-channel');
+            if (!subscribedChannels.has(channel)) {
+                subscribedChannels.add(channel);
+                balance += 10000;
+                balanceElement.textContent = balance;
+                window.open(button.previousElementSibling.querySelector('a').href, '_blank');
+            } else {
+                alert("Вы уже подписались на этот канал и получили награду.");
+            }
+        });
+    });
+});
